20d669b6cfab05a13d4cd7d928bbc793394530d0
Mathis Liens <mathis.liens@etu.minesparis.psl.eu>
2024-10-29 17:37:40 +0100

    Création d'un graph et envoie de celui-ci Vous trouverez les modifs dans stats.py

--- stats.py
+++ stats.py
@@ -10,3 +10,12 @@ print("Nombre de lignes :", len(df))
 
 valeurs_manquante = df.isna().sum()
 print("Valeurs manquantes : \n",valeurs_manquante)
+
+x = df.groupby("Type")["Magnitude"].describe()
+
+#df.groupby("Type")["Magnitude"].mean().plot(yerr = [df.groupby("Type")["Magnitude"].quantile(0.25),df.groupby("Type")["Magnitude"].quantile(0.75)]).set_ylim(0,7)
+graf = x["mean"].plot(yerr = [x["mean"]-x["25%"], x["75%"]-x["mean"]], ylabel = "Magnitude",kind ="bar", xlabel="Source de la secousse", title="Magnitude en fonction de la source de la secousse")
+graf.set_xticks([0,1,2,3],x.index, rotation = "horizontal")
+graf.set_ylim(bottom = 0, top =7.5)
+
+plt.show()
