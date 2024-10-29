410359ff317082180a8f16a975dd4802a8860ae6
Mathis Liens <mathis.liens@etu.minesparis.psl.eu>
2024-10-29 17:43:19 +0100

    Ajout de petits commentaires (pas de modif code)

--- stats.py
+++ stats.py
@@ -13,9 +13,11 @@ print("Valeurs manquantes : \n",valeurs_manquante)
 
 x = df.groupby("Type")["Magnitude"].describe()
 
-#df.groupby("Type")["Magnitude"].mean().plot(yerr = [df.groupby("Type")["Magnitude"].quantile(0.25),df.groupby("Type")["Magnitude"].quantile(0.75)]).set_ylim(0,7)
+#Création du graph sur la magnitude en fonction de la source
 graf = x["mean"].plot(yerr = [x["mean"]-x["25%"], x["75%"]-x["mean"]], ylabel = "Magnitude",kind ="bar", xlabel="Source de la secousse", title="Magnitude en fonction de la source de la secousse")
 graf.set_xticks([0,1,2,3],x.index, rotation = "horizontal")
 graf.set_ylim(bottom = 0, top =7.5)
 
 plt.show()
+
+#C'est vraiment dur de trouver des idées de trucs à faire...
